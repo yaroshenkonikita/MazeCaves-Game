9dd9cedab0fbfe8c660264907a8fa1849fe87225
tokimikichika <yaroshenkonikita4@gmail.com>
2023-05-01 22:39:09 +0300

    add tests

--- src/tests/maze_tests.cc
+++ src/tests/maze_tests.cc
@@ -17,40 +17,40 @@ TEST(Maze,
 }
 
 TEST(Maze, parsing_file_no_exist) {
-    s21::Maze maze;
-    maze.LoadFromFile("tests/test.txt");
-    EXPECT_ANY_THROW(maze.LoadFromFile("no_file.txt"));
-    EXPECT_TRUE(maze.IsValidMaze());
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test.txt");
+  EXPECT_ANY_THROW(maze.LoadFromFile("no_file.txt"));
+  EXPECT_TRUE(maze.IsValidMaze());
 }
 
 TEST(Maze, invalid_data_parsing) {
-    s21::Maze maze;
-    maze.LoadFromFile("tests/test.txt");
-    EXPECT_ANY_THROW(maze.LoadFromFile("tests/test_invalid_data.txt"));
-    EXPECT_TRUE(maze.IsValidMaze());
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test.txt");
+  EXPECT_ANY_THROW(maze.LoadFromFile("tests/test_invalid_data.txt"));
+  EXPECT_TRUE(maze.IsValidMaze());
 }
 
 TEST(Maze, invalid_data_parsing2) {
-    s21::Maze maze;
-    maze.LoadFromFile("tests/test.txt");
-    EXPECT_ANY_THROW(maze.LoadFromFile("tests/test_invalid_data2.txt"));
-    EXPECT_TRUE(maze.IsValidMaze());
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test.txt");
+  EXPECT_ANY_THROW(maze.LoadFromFile("tests/test_invalid_data2.txt"));
+  EXPECT_TRUE(maze.IsValidMaze());
 }
 
 TEST(Maze, invalid_data_parsing3) {
-    s21::Maze maze;
-    maze.LoadFromFile("tests/test.txt");
-    EXPECT_ANY_THROW(maze.LoadFromFile("tests/test_invalid_data3.txt"));
-    EXPECT_TRUE(maze.IsValidMaze());
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test.txt");
+  EXPECT_ANY_THROW(maze.LoadFromFile("tests/test_invalid_data3.txt"));
+  EXPECT_TRUE(maze.IsValidMaze());
 }
 
 TEST(Maze, save) {
-    s21::Maze maze;
-    maze.LoadFromFile("tests/test.txt");
-    maze.SaveToFile("check_test_save.txt");
-    s21::Maze copy;
-    copy.LoadFromFile("check_test_save.txt");
-    EXPECT_TRUE(copy.GetMatrix() == maze.GetMatrix());
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test.txt");
+  maze.SaveToFile("check_test_save.txt");
+  s21::Maze copy;
+  copy.LoadFromFile("check_test_save.txt");
+  EXPECT_TRUE(copy.GetMatrix() == maze.GetMatrix());
 }
 
 TEST(Maze, check_on_perfect_maze_generate) {  // always random
@@ -98,6 +98,33 @@ TEST(MazeSolver, escape_from_maze) {
   maze.LoadFromFile("tests/test.txt");
   s21::MazeSolver solver;
   solver.SolveMaze({0, 0}, {9, 9}, maze);
+  EXPECT_TRUE(loc == solver.GetLocations());
+}
+
+TEST(MazeSolver, escape_from_maze2) {
+  std::vector<s21::Location> loc{
+      {2, 0}, {1, 0}, {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5},
+      {0, 6}, {0, 7}, {0, 8}, {0, 9}, {1, 9}, {2, 9}, {3, 9}, {3, 8},
+      {4, 8}, {4, 9}, {5, 9}, {6, 9}, {6, 8}, {6, 7}, {6, 6}, {6, 5},
+      {5, 5}, {5, 4}, {4, 4}, {4, 3}, {4, 2}, {4, 1}, {4, 0}, {3, 0},
+      {3, 1}, {3, 2}, {3, 3}, {2, 3}, {2, 2}, {2, 1}};
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test2.txt");
+  s21::MazeSolver solver;
+  solver.SolveMaze({2, 0}, {2, 1}, maze);
+  EXPECT_TRUE(loc == solver.GetLocations());
+}
+
+TEST(MazeSolver, escape_from_maze3) {
+  std::vector<s21::Location> loc{
+      {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {1, 5}, {2, 5},
+      {2, 4}, {2, 3}, {3, 3}, {4, 3}, {4, 4}, {4, 5}, {5, 5}, {5, 6},
+      {6, 6}, {6, 7}, {7, 7}, {7, 8}, {7, 9}, {6, 9}, {5, 9}, {4, 9},
+      {4, 8}, {3, 8}, {2, 8}, {2, 9}, {1, 9}, {1, 8}, {1, 7}, {1, 6}};
+  s21::Maze maze;
+  maze.LoadFromFile("tests/test3.txt");
+  s21::MazeSolver solver;
+  solver.SolveMaze({0, 0}, {1, 6}, maze);
   EXPECT_TRUE(loc == solver.GetLocations());
 }
 
